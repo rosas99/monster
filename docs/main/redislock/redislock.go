c32d7a5469b0300e33544dcdc5a27f303805f8aa
Mervyn <starry99c@163.com>
2024-08-06 17:55:40 +0800

    debug

--- docs/main/redislock/redislock.go
+++ docs/main/redislock/redislock.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"context"
+	"errors"
 	"github.com/redis/go-redis/v9"
-
+	"github.com/rosas99/monster/pkg/log"
 	"github.com/zeromicro/go-zero/core/stringx"
-	"log"
 	"math/rand"
 	"sync/atomic"
 	"time"
@@ -18,7 +18,6 @@ const (
 )
 
 var (
-	// 这里使用 go-redis 的 Lua 脚本功能
 	lockScript = redis.NewScript(`
 		if redis.call("GET", KEYS[1]) == ARGV[1] then
 			redis.call("SET", KEYS[1], ARGV[1], "PX", ARGV[2])
@@ -27,6 +26,7 @@ var (
 			return redis.call("SET", KEYS[1], ARGV[1], "NX", "PX", ARGV[2])
 		end
 	`)
+
 	delScript = redis.NewScript(`
 		if redis.call("GET", KEYS[1]) == ARGV[1] then
 			return redis.call("DEL", KEYS[1])
@@ -36,7 +36,6 @@ var (
 	`)
 )
 
-// RedisLock 是使用 Redis 实现的分布式锁。
 type RedisLock struct {
 	store   *redis.Client
 	seconds uint32
@@ -48,37 +47,50 @@ func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
-// NewRedisLock 返回一个新的 RedisLock 实例。
 func NewRedisLock(store *redis.Client, key string) *RedisLock {
 	return &RedisLock{
 		store:   store,
 		key:     key,
-		id:      stringx.Randn(randomLen), // 假设 stringx.Randn 是可用的
-		seconds: 30,                       // 默认超时时间
+		id:      stringx.Randn(randomLen),
+		seconds: 30,
 	}
 }
 
-// AcquireCtx 尝试获取锁，使用提供的上下文。
 func (rl *RedisLock) AcquireCtx(ctx context.Context) (bool, error) {
 	seconds := atomic.LoadUint32(&rl.seconds)
 	val, err := lockScript.Run(ctx, rl.store, []string{rl.key}, rl.id, int64(seconds*millisPerSecond+tolerance)).Result()
-	if err != nil {
-		log.Printf("Error on acquiring lock for %s: %v", rl.key, err)
+	if errors.Is(err, redis.Nil) {
+		return false, nil
+	} else if err != nil {
+		log.Errorf("Error on acquiring lock for %s, %s", rl.key, err.Error())
 		return false, err
+	} else if val == nil {
+		return false, nil
 	}
-	return val == "OK", nil
+
+	reply, ok := val.(string)
+	if ok && reply == "OK" {
+		return true, nil
+	}
+
+	log.Errorf("Unknown reply when acquiring lock for %s: %v", rl.key, val)
+	return false, nil
 }
 
-// ReleaseCtx 尝试释放锁，使用提供的上下文。
 func (rl *RedisLock) ReleaseCtx(ctx context.Context) (bool, error) {
 	val, err := delScript.Run(ctx, rl.store, []string{rl.key}, rl.id).Result()
 	if err != nil {
 		return false, err
 	}
-	return val.(int64) == 1, nil
+
+	reply, ok := val.(int64)
+	if !ok {
+		return false, nil
+	}
+
+	return reply == 1, nil
 }
 
-// SetExpire 设置锁的超时时间（秒）。
 func (rl *RedisLock) SetExpire(seconds uint32) {
 	atomic.StoreUint32(&rl.seconds, seconds)
 }
